bd1bc268c43d28742ae1214966cc15fc672a45c2
RedFly <maxences427@gmail.com>
2025-11-24 17:28:34 +0100

    clear

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/divizion-launcher-earth-view/', // Base URL for GitHub Pages
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
-});
